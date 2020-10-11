86e571e26cecc5cd7d34fa3a364732a3484d288c
shangsuru <helm.henry99@gmail.com>
2020-10-11 09:17:22 +0200

    Implemented store and list function

--- music/play-music.py
+++ music/play-music.py
@@ -1,10 +1,20 @@
 import argparse
 from player import MusicPlayer
 
-MUSIC_TEXT_FILE = "music.txt"
+"""
+Text file where all songs are stored in the format
+"[index];[link];[description]"
+"""
+MUSIC_FILE = "music.txt"
 
-parser = argparse.ArgumentParser(description="Open a browser window with music")
 
+# ==============================
+# Define command line arguments
+# ==============================
+
+parser = argparse.ArgumentParser(
+    description="Open a browser window and listen to music"
+)
 parser.add_argument("-p", "--play", nargs=None, type=int, help="Play a song")
 parser.add_argument(
     "-r",
@@ -12,27 +22,41 @@ parser.add_argument(
     nargs="+",
     metavar="N",
     type=int,
-    help="Remove song from music list",
+    help="Remove song from music collection",
 )
 parser.add_argument(
-    "-a", "--add", nargs=2, metavar=("LINK", "DESCRIPTION"), help="Add new music"
+    "-a",
+    "--add",
+    nargs=2,
+    metavar=("LINK", "DESCRIPTION"),
+    help="Add new song to collection",
 )
-parser.add_argument("-l", "--list", action="store_true", help="Show music list")
+parser.add_argument("-l", "--list", action="store_true", help="Show music collection")
+
+
+# =============================
+# Execute command
+# =============================
 
 args = parser.parse_args()
-
-songs = MusicPlayer(MUSIC_TEXT_FILE)
+songs = MusicPlayer(MUSIC_FILE)
 
 if args.play:
     songs.play(args.play)
+
 elif args.remove:
     songs.remove(args.remove)
+
 elif args.add:
     link, description = args.add
     songs.store(link, description)
-    pass
+
 elif args.list:
-    songs.show()
+    songs = songs.show()
+    for song in songs:
+        index, link, description = song
+        print(f"[{index}] {link} - {description}")
+
 else:
     random = 0
     songs.play(random)
--- music/player.py
+++ music/player.py
@@ -1,15 +1,36 @@
+"""
+Can read and write and delete songs in the format
+"[index];[link];[description]" to a specified
+text file. 
+
+Opens the selected song in a new full-screen browser window.
+"""
+
+
 class MusicPlayer:
     def __init__(self, path):
-        self.music_file = path
+        self.path = path
+        self.songs = []
+        with open(path) as musicFile:
+            lines = musicFile.readlines()
+            for line in lines:
+                self.songs.append(tuple(line.split(";")))
+
+    def play(self, index):
+        pass
 
     def store(self, link, description):
-        pass
+        index = len(self.songs) + 1
+        link.replace(";", "")
+        description.replace(";", ".")
 
-    def show(self):
-        pass
+        self.songs.append((index, link, description))
+        with open(self.path, "a") as musicFile:
+            musicFile.write(f"{index};{link};{description}")
+            musicFile.write("\n")
 
     def remove(self, indexes):
         pass
 
-    def play(self, index):
-        pass
+    def show(self):
+        return self.songs
